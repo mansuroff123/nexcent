2291b58d308cdc93e69d0aa84328a49319390703
mansuroff123 <mansuroff145@gmail.com>
2025-05-29 19:05:14 +0500

    project finished and some js function added

--- assets/main/js/index.js
+++ assets/main/js/index.js
@@ -2,5 +2,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navlinks = document.querySelectorAll('.nav-link')
 
-    
+    navlinks.forEach(link => {
+        link.addEventListener("click", function() {
+            navlinks.forEach(l => l.classList.remove("active"))
+
+            this.classList.add('active')
+        })
+    })
+})
+
+// navbar scrolling shadow effect
+const navbar = document.querySelector("nav.my_nav")
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('shadow')
+    } else {
+        navbar.classList.remove('shadow')
+    }
 })
